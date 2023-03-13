5db99a0f25a73f99303f2ee471066c720ecaee2b
Gitisfun <christianvandenputtelaar@gmail.com>
2023-03-13 23:58:06 +0100

    Add adsense

--- scores/nuxt.config.ts
+++ scores/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@pinia/nuxt", "nuxt-simple-sitemap"],
+  modules: ["@pinia/nuxt", "nuxt-simple-sitemap", "@nuxtjs/google-adsense"],
   sitemap: {
     hostname: "https://kavvv-uitslagen.be",
   },
@@ -9,5 +9,8 @@ export default defineNuxtConfig({
       crawlLinks: true,
       routes: ["/"],
     },
+  },
+  "google-adsense": {
+    id: "ca-pub-9928832567373030",
   },
 });
