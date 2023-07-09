dd63fd65164f69106d85750e76f657487a4c4274
Gitisfun <christianvandenputtelaar@gmail.com>
2023-07-09 19:46:02 +0200

    Add dynamic routing for seo

--- scores/nuxt.config.ts
+++ scores/nuxt.config.ts
@@ -3,6 +3,33 @@ export default defineNuxtConfig({
   modules: ["@pinia/nuxt", "nuxt-simple-sitemap", "@nuxtjs/google-adsense", "@nuxtjs/device"],
   sitemap: {
     hostname: "https://kavvv-uitslagen.be",
+
+    urls: async () => {
+      const urls = [
+        //'https://scraper-scores-antwerpen.herokuapp.com/api/clubs',
+        "https://scraper-scores.herokuapp.com/api/clubs",
+        //"https://scraper-scores.herokuapp.com/api/clubs",
+      ];
+
+      try {
+        const requests = urls.map((url) => fetch(url));
+        const responses = await Promise.all(requests);
+
+        const responseData = await Promise.all(responses.map((response) => response.json()));
+
+        // @ts-ignore
+        return responseData[0].map((club) => ({
+          loc: `/blog/${club.slug}`,
+          changefreq: "weekly",
+          priority: 0.8,
+        }));
+        // Do something with the response data
+      } catch (error) {
+        // Handle any errors that occurred during the requests
+        return [];
+      }
+      //return [...responseData[0], ...responseData[1].map((club) => ({
+    },
   },
   nitro: {
     prerender: {
