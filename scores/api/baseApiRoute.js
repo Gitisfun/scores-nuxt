b433de5c872a1c1dc4c6a839d7813e7174e6d482
Gitisfun <christianvandenputtelaar@gmail.com>
2023-09-16 21:58:29 +0200

    Small fix

--- scores/api/baseApiRoute.js
+++ scores/api/baseApiRoute.js
@@ -4,7 +4,6 @@ const baseApiRoute = (province) => {
   if (province === ANTWERPEN) return "https://scraper-scores-antwerpen.herokuapp.com/api";
   if (province === BRABANT) return "https://scraper-scores.herokuapp.com/api";
   return "https://scraper-scores.herokuapp.com/api";
-
 };
 
 export default baseApiRoute;
