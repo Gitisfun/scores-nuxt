e0c5119d65ca5c2a82306b4da523963999492c4d
Christian <christian.vandeputtelaar@bosa.fgov.be>
2025-08-20 20:16:53 +0200

    Test commit

--- scores/pages/index.vue
+++ scores/pages/index.vue
@@ -3,17 +3,10 @@
     <Header v-if="isVisible" title="KAVVV Uitslagen" />
     <div class="home-box">
       <div v-if="isVisible">
-        <h2 class="home-info-box">
-          Alle voetbal uitslagen van de Koninklijke Algemene Vereniging van
-          Vriendenclubs Antwerpen, Vlaams-Brabant & Oost-Vlaanderen. Vind hier
-          de uitslagen van jouw lokale club!
-        </h2>
+        <h2 class="home-info-box">Alle voetbal uitslagen van de Koninklijke Algemene Vereniging van Vriendenclubs Antwerpen, Vlaams-Brabant & Oost-Vlaanderen. Vind hier de uitslagen van jouw lokale club!</h2>
         <h4>Kies een provincie</h4>
         <NavigationItem title="Antwerpen" :route="ANTWERPEN_ROUTE" />
-        <NavigationItem
-          title="Vlaams-Brabant & Oost-Vlaanderen"
-          :route="BRABANT_ROUTE"
-        />
+        <NavigationItem title="Vlaams-Brabant & Oost-Vlaanderen" :route="BRABANT_ROUTE" />
       </div>
       <SearchClub v-if="isEnabled" :setSearching="setSearching" />
     </div>
@@ -21,7 +14,7 @@
 </template>
 
 <script setup>
-import { ANTWERPEN_ROUTE, BRABANT_ROUTE } from "~~/logic/constants/routes";
+import { ANTWERPEN_ROUTE, BRABANT_ROUTE } from '~~/logic/constants/routes';
 
 const isEnabled = false;
 const { isMobile } = useDevice();
@@ -36,53 +29,51 @@ const isVisible = computed(() => {
 const setSearching = (value) => (isSearching.value = value);
 
 useHead({
-  title: "KAVVV uitslagen",
+  title: 'KAVVV uitslagen',
   meta: [
     {
-      name: "description",
-      content:
-        "Alle voetbal uitslagen van de Koninklijke Algemene Vereniging van Vriendenclubs Antwerpen, Vlaams-Brabant & Oost-Vlaanderen. Vind hier de uitslagen van jouw lokale club!",
+      name: 'description',
+      content: 'Alle voetbal uitslagen van de Koninklijke Algemene Vereniging van Vriendenclubs Antwerpen, Vlaams-Brabant & Oost-Vlaanderen. Vind hier de uitslagen van jouw lokale club!',
     },
     {
-      name: "google-site-verification",
-      content: "i-d5dcGOmI5iffIB3u5T1hYZjLtJjcaCTl2xrbYTFx8",
+      name: 'google-site-verification',
+      content: 'i-d5dcGOmI5iffIB3u5T1hYZjLtJjcaCTl2xrbYTFx8',
     },
-    { property: "og:title", content: "KAVVV Uitslagen" },
+    { property: 'og:title', content: 'KAVVV Uitslagen' },
     {
-      property: "og:description",
-      content:
-        "Alle voetbal uitslagen van de Koninklijke Algemene Vereniging van Vriendenclubs Antwerpen, Vlaams-Brabant & Oost-Vlaanderen. Vind hier de uitslagen van jouw lokale club!",
+      property: 'og:description',
+      content: 'Alle voetbal uitslagen van de Koninklijke Algemene Vereniging van Vriendenclubs Antwerpen, Vlaams-Brabant & Oost-Vlaanderen. Vind hier de uitslagen van jouw lokale club!',
     },
-    { property: "og:type", content: "website" },
-    { property: "og:url", content: "https://kavvv-uitslagen.be" },
-    { property: "og:locale", content: "nl_Be" },
+    { property: 'og:type', content: 'website' },
+    { property: 'og:url', content: 'https://kavvv-uitslagen.be' },
+    { property: 'og:locale', content: 'nl_Be' },
     {
-      property: "og:image",
-      content: "https://kavvv-uitslagen.be/mstile-144x144.png",
+      property: 'og:image',
+      content: 'https://kavvv-uitslagen.be/mstile-144x144.png',
     },
   ],
-  htmlAttrs: { lang: "nl" },
-  link: [{ rel: "canonical", href: "https://kavvv-uitslagen.be" }],
+  htmlAttrs: { lang: 'nl' },
+  link: [{ rel: 'canonical', href: 'https://kavvv-uitslagen.be' }],
   link: [
     {
-      rel: "icon",
-      type: "image/png",
-      sizes: "32x32",
-      href: "/favicon-32x32.png",
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicon-32x32.png',
     },
     {
-      rel: "icon",
-      type: "image/png",
-      sizes: "16x16",
-      href: "/favicon-16x16.png",
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicon-16x16.png',
     },
   ],
 });
 
 const { gtag } = useGtag();
-gtag("home_page", "visit", {
-  app_name: "KAVVV",
-  screen_name: "Home",
+gtag('home_page', 'visit', {
+  app_name: 'KAVVV',
+  screen_name: 'Home',
 });
 </script>
 
